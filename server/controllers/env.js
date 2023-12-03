879b56ec684ff96691a5172a16e02ba8a0a13fa4
himani15s <hinusharma124@gmail.com>
2023-12-03 19:44:01 +0530

    Remove folder_name from version control

--- server/controllers/env.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    EMAIL:"sharmahinu9166@gmail.com",
-    PASSWORD:"xvhimani9166926685",
-}
--- server/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-app.get('/', function (req, res) {
-    res.send("<h1>Welcome to my server</h1>");
-
-});
